36fabd809cf7c1b3236948fd0cfe183e490575c1
ohara <yohara8814@gmail.com>
2020-05-05 17:45:57 +0900

    Create static-pages

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,30 @@
+class QuestionsController < ApplicationController
+    def index
+        @text = 'テキストテキスト'
+    end
+
+    def show
+        @question = Question.find(params[:id])
+    end
+
+    def new
+        @question = Question.new
+    end
+
+    def create
+        #入力フォームからPOSTされた値を使ってインスタンスを作成し、@questionに代入
+        @question = Question.new(question_params)
+        if @question.save
+            flash[:notice] = "保存しました"
+            redirect_to("/questions/#{@question.id}") #DBへの保存が成功したら新規作成ページに戻る
+        else
+            flash.now[:alert] = "保存に失敗しました"
+            render("questions/new")
+        end
+    end
+
+    private 
+    def question_params
+        params.require(:question).permit(:title, :body)
+    end
+end
