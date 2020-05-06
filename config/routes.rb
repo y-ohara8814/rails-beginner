fc3d7293f97b76fb3823413a7997edb55e103e2a
ohara <yohara8814@gmail.com>
2020-05-06 18:50:49 +0900

    create login-system and user-detail pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
+  get "/users/:id", to: "users#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'questions#index'
   get "/questions", to: "questions#index" 
