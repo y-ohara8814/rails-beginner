3dfae533d06895c6ee6a702685d9e6615e25c281
ohara <yohara8814@gmail.com>
2020-05-10 19:13:13 +0900

    create route to mypage

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,7 +2,11 @@ class QuestionsController < ApplicationController
     before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
     before_action :ensure_correct_user, only: [:edit, :update, :destroy]
     def index
-        @text = '質問インデックスページ'
+        if user_signed_in?
+            @text = 'ようこそ、'+ current_user.name + 'さん'
+        else
+            @text = 'ご利用になるには、新規登録またはログインしてください'
+        end
     end
 
     def show
